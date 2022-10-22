d642796a67fa12b0a9d12be3628ff182288734da
Robert Dober <robert.dober@gmail.com>
2022-10-22 22:08:03 +0200

    scaffolded

---
+++ lib/minipeg.ex
@@ -0,0 +1,18 @@
+defmodule Minipeg do
+  @moduledoc """
+  Documentation for `Minipeg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Minipeg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/minipeg_test.exs
@@ -0,0 +1,8 @@
+defmodule MinipegTest do
+  use ExUnit.Case
+  doctest Minipeg
+
+  test "greets the world" do
+    assert Minipeg.hello() == :world
+  end
+end
